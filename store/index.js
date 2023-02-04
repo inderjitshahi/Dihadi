53801d8f8954d6eebaab74788e67fcde302fcc9f
Inderjit Shahi <inderjitshahi_mc20a7_13@dtu.ac.in>
2023-02-04 20:53:58 +0530

    initialized

--- store/index.js
+++ store/index.js
@@ -1,17 +1,16 @@
 import { configureStore} from '@reduxjs/toolkit';
-import complaintSlice from './complaintSlice';
+// import complaintSlice from './complaintSlice';
 import userSlice from './userSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
 import { combineReducers } from '@reduxjs/toolkit';
-import { createWrapper,HYDRATE } from 'next-redux-wrapper';
 const persistConfig={
     key:'root',
     version:1,
     storage 
 }
 const reducer=combineReducers({
-    complaints: complaintSlice.reducer,
+    // complaints: complaintSlice.reducer,
     user:userSlice.reducer,
 });
 
@@ -20,6 +19,5 @@ const persistedReducer=persistReducer(persistConfig,reducer);
 const store = configureStore({
     reducer: persistedReducer,
 });
-// export const wrapper=createWrapper(store);
 
 export default store;
